ae2d1d4c7cba7ce069dea9dde08cc6161f5b76d4
AmrMohamad <amr.mohamed124124@gmail.com>
2023-02-07 21:36:03 +0200

    initializing Magic 8 Ball app

---
+++ Magic 8 Ball iOS16/Magic 8 Ball iOS16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball iOS16
+//
+//  Created by Amr Mohamad on 07/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
