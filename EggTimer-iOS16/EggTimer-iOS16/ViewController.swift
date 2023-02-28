e0e6b716ea6dd1c79a93df0c728478f9fc122b2d
AmrMohamad <amr.mohamed124124@gmail.com>
2023-02-28 13:37:28 +0200

    initialize EggTimer app

---
+++ EggTimer-iOS16/EggTimer-iOS16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer-iOS16
+//
+//  Created by Amr Mohamad on 28/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
