25ccf1ba8e66df11ed5af4fd88b7689e7ceb9172
AmrMohamad <amr.mohamed124124@gmail.com>
2023-03-05 18:46:36 +0200

    .

--- EggTimer-iOS16/EggTimer-iOS16/ViewController.swift
+++ EggTimer-iOS16/EggTimer-iOS16/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let softTime = 5
+    let medTime = 7
+    let hardTime = 12
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        print(sender.currentTitle!)
+        if sender.currentTitle! == "Soft"{
+            print(softTime)
+        } else if sender.currentTitle! == "Medium"{
+            print(medTime)
+        } else{
+            print(hardTime)
+        }
+    }
+    
 }
 
