0831854b1e8011c126e636ed631d517c9a8daf6d
AmrMohamad <amr.mohamed124124@gmail.com>
2023-03-10 21:45:14 +0200

    complate egg timer project

--- EggTimer-iOS16/EggTimer-iOS16/ViewController.swift
+++ EggTimer-iOS16/EggTimer-iOS16/ViewController.swift
@@ -8,23 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let softTime = 5
-    let medTime = 7
-    let hardTime = 12
-
+    @IBOutlet weak var prograssOfEgg: UIProgressView!
+    @IBOutlet weak var isDone: UILabel!
+    let eggTimes = ["Soft": 300,"Medium":420,"Hard":720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        prograssOfEgg.progress = 0.0
     }
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle!)
-        if sender.currentTitle! == "Soft"{
-            print(softTime)
-        } else if sender.currentTitle! == "Medium"{
-            print(medTime)
-        } else{
-            print(hardTime)
+        timer.invalidate()
+        totalTime = eggTimes[sender.currentTitle!]!
+        prograssOfEgg.progress = 0.0
+        secondsPassed = 0
+        isDone.text = sender.currentTitle!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil , repeats: true)
+
+        
+    }
+    @objc func updateTimer(){
+        if secondsPassed<totalTime{
+            secondsPassed += 1
+            prograssOfEgg.progress = Float(secondsPassed)/Float(totalTime)
+            
+            print(secondsPassed)
+        }else{
+            timer.invalidate()
+            isDone.text = "Done!"
         }
     }
     
