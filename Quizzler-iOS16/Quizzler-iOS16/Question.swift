f727352fcb68aac56d8370443605fb181f203c95
AmrMohamad <amr.mohamed124124@gmail.com>
2023-03-19 18:31:59 +0200

    add functionality to Quizzler app

---
+++ Quizzler-iOS16/Quizzler-iOS16/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  Quizzler-iOS16
+//
+//  Created by Amr Mohamad on 18/03/2023.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let answer: String
+}
--- Quizzler-iOS16/Quizzler-iOS16/ViewController.swift
+++ Quizzler-iOS16/Quizzler-iOS16/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
 
+    let quiz = [
+        Question(questionText: "4 + 2 = 6 ?", answer: "True"),
+        Question(questionText: "Five - Three is greater than one", answer: "True"),
+        Question(questionText: "3 + Eight is < 10", answer: "False")
+    ]
+    
+    var currentQuestionNumber: Int = 0
+    var progressPercentage: Float = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +33,33 @@ class ViewController: UIViewController {
         falseButton.layer.borderWidth = CGFloat(5)
         falseButton.layer.cornerRadius = CGFloat(25)
         falseButton.layer.borderColor = UIColor.systemGray2.cgColor
+        
+        updateUI()
     }
 
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let rightAnswer = quiz[currentQuestionNumber].answer
+        
+        if userAnswer == rightAnswer {
+            currentQuestionNumber += 1
+            progressPercentage = Float(currentQuestionNumber)/Float(quiz.count)
+            updateUI()
+        }
+        
+    }
+    
+    func updateUI(){
+        if currentQuestionNumber < quiz.count{
+            questionLabel.text = quiz[currentQuestionNumber].questionText
+            progressBar.progress = progressPercentage
+        }else{
+            currentQuestionNumber = 0
+            progressPercentage = 0.0
+            questionLabel.text = quiz[currentQuestionNumber].questionText
+            progressBar.progress = progressPercentage
+        }
         
     }
 }
