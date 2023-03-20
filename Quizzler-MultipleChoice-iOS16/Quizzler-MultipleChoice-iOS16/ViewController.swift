78b64f23918169759d5ca6ec5ff5e0e3d2da8b52
AmrMohamad <amr.mohamed124124@gmail.com>
2023-03-20 22:57:40 +0200

    initailize Quizzler-MultipleChoice-iOS16 project

---
+++ Quizzler-MultipleChoice-iOS16/Quizzler-MultipleChoice-iOS16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler-MultipleChoice-iOS16
+//
+//  Created by Amr Mohamad on 20/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
