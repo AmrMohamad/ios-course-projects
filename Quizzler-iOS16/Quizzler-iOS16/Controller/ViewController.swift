c0c117cff8fa395bee871a8a4f103a7b535871cf
AmrMohamad <amr.mohamed124124@gmail.com>
2023-03-20 18:16:02 +0200

    complete Quizzler app

--- Quizzler-iOS16/Quizzler-iOS16/Controller/ViewController.swift
+++ Quizzler-iOS16/Quizzler-iOS16/Controller/ViewController.swift
@@ -9,62 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
-
-    let quiz = [
-        Question(
-            questionText: "A slug's blood is green.",
-            answer: "True"
-        ),
-        Question(
-            questionText: "Approximately one quarter of human bones are in the feet.",
-            answer: "True"
-        ),
-        Question(
-            questionText: "The total surface area of two human lungs is approximately 70 square metres.",
-            answer: "True"
-        ),
-        Question(
-            questionText: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.",
-            answer: "True"
-        ),
-        Question(
-            questionText: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.",
-            answer: "False"
-        ),
-        Question(
-            questionText: "It is illegal to pee in the Ocean in Portugal.",
-            answer: "True"
-        ),
-        Question(
-            questionText: "You can lead a cow down stairs but not up stairs.",
-            answer: "False"
-        ),
-        Question(
-            questionText: "Google was originally called 'Backrub'.",
-            answer: "True"
-        ),
-        Question(
-            questionText: "Buzz Aldrin's mother's maiden name was 'Moon'.",
-            answer: "True"
-        ),
-        Question(
-            questionText: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.",
-            answer: "False"
-        ),
-        Question(
-            questionText: "No piece of square dry paper can be folded in half more than 7 times.",
-            answer: "False"
-        ),
-        Question(
-            questionText: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.",
-            answer: "True"
-        )
-    ]
     
-    var currentQuestionNumber: Int = 0
+    var quiz = QuizOperations()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -83,25 +33,23 @@ class ViewController: UIViewController {
 
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        let userAnswer = sender.currentTitle!
-        let rightAnswer = quiz[currentQuestionNumber].answer
+       let result =  quiz.checkAnswer(user: sender.currentTitle!)
         
-        if userAnswer == rightAnswer {
+        if result {
             sender.backgroundColor = UIColor.green
         }else{
             sender.backgroundColor = UIColor.red
         }
-        if currentQuestionNumber+1 < quiz.count {
-            currentQuestionNumber += 1
-        }else{
-            currentQuestionNumber = 0
-        }
+        
+        quiz.nextQuestion()
+        
         Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
     @objc func updateUI(){
-        questionLabel.text = quiz[currentQuestionNumber].questionText
-        progressBar.progress = Float(currentQuestionNumber + 1)/Float(quiz.count)
+        questionLabel.text = quiz.getQuestionText()
+        progressBar.progress = quiz.getProgress()
+        scoreLabel.text = "Score: \(quiz.getScore())"
         falseButton.backgroundColor = UIColor.clear
         trueButton.backgroundColor = UIColor.clear
     }
