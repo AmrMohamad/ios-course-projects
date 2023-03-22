39010596d3d1fe8ba1b94d93c40a01294f6f8b32
AmrMohamad <amr.mohamed124124@gmail.com>
2023-03-23 00:51:07 +0200

    initailize UI Quizzler-MultipleChoice-iOS16 project

--- Quizzler-MultipleChoice-iOS16/Quizzler-MultipleChoice-iOS16/Controller/MainQuizViewController.swift
+++ Quizzler-MultipleChoice-iOS16/Quizzler-MultipleChoice-iOS16/Controller/MainQuizViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainQuizViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Quizzler-MultipleChoice-iOS16/Quizzler-MultipleChoice-iOS16/View/QuizUI.swift
@@ -0,0 +1,20 @@
+//
+//  QuizUI.swift
+//  Quizzler-MultipleChoice-iOS16
+//
+//  Created by Amr Mohamad on 22/03/2023.
+//
+
+import UIKit
+
+class QuizUI: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
