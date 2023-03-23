a268ec025685837dbbfebb5389629966bb05d610
AmrMohamad <amr.mohamed124124@gmail.com>
2023-03-23 15:15:06 +0200

    full initailized UI Quizzler-MultipleChoice-iOS16 project

--- Quizzler-MultipleChoice-iOS16/Quizzler-MultipleChoice-iOS16/View/QuizUI.swift
+++ Quizzler-MultipleChoice-iOS16/Quizzler-MultipleChoice-iOS16/View/QuizUI.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class QuizUI {
+    
+    let scoreLabel : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        label.text = "Score: "
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    let questionTextLabel : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        label.text = "Question Text"
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    
     let choose1Button : UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Test", for: .normal)
+        button.setTitle("Choose 1", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 25, weight: .medium)
         button.layer.borderWidth = CGFloat(5)
         button.layer.cornerRadius = CGFloat(25)
         button.layer.borderColor = UIColor.systemGray2.cgColor
@@ -21,7 +45,9 @@ class QuizUI {
     let choose2Button : UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Test", for: .normal)
+        button.setTitle("Choose 2", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 25, weight: .medium)
         button.layer.borderWidth = CGFloat(5)
         button.layer.cornerRadius = CGFloat(25)
         button.layer.borderColor = UIColor.systemGray2.cgColor
@@ -31,7 +57,9 @@ class QuizUI {
     let choose3Button : UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Test", for: .normal)
+        button.setTitle("Choose 3", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 25, weight: .medium)
         button.layer.borderWidth = CGFloat(5)
         button.layer.cornerRadius = CGFloat(25)
         button.layer.borderColor = UIColor.systemGray2.cgColor
@@ -43,6 +71,17 @@ class QuizUI {
         image.translatesAutoresizingMaskIntoConstraints = false
         image.contentMode = .scaleToFill
         return image
+    }()
+    
+    let progressBar: UIProgressView = {
+        let bar = UIProgressView()
+        bar.translatesAutoresizingMaskIntoConstraints = false
+        bar.progressViewStyle = .bar
+        bar.progress = 0.1
+        bar.contentMode = .scaleToFill
+        bar.progressTintColor = UIColor(named: "progressBarColor")
+        bar.trackTintColor = .white
+        return bar
     }()
 
 }
