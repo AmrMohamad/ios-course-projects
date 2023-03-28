121cad72585167dde317a439e79d54c3f4272923
AmrMohamad <amr.mohamed124124@gmail.com>
2023-03-28 18:00:54 +0200

    Create BMI-Calculator-UIKit-iOS16 project

---
+++ BMI-Calculator-UIKit-iOS16/BMI-Calculator-UIKit-iOS16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Calculator-UIKit-iOS16
+//
+//  Created by Amr Mohamad on 28/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
