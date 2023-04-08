822bcd9019df7adf55fa273aff546ff900d8a7ba
AmrMohamad <amr.mohamed124124@gmail.com>
2023-04-08 16:44:27 +0200

    initialize Tipsy-UIKit-iOS16 project and Tipsy-iOS16 project

---
+++ Tipsy-iOS16/Tipsy-iOS16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy-iOS16
+//
+//  Created by Amr Mohamad on 08/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
