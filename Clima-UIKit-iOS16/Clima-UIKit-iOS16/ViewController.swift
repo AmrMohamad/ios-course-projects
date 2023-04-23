e809ed0abdfacd5f7e87c8eae78c986e451c8687
AmrMohamad <amr.mohamed124124@gmail.com>
2023-04-23 17:45:49 +0200

    initializing Clima project

---
+++ Clima-UIKit-iOS16/Clima-UIKit-iOS16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clima-UIKit-iOS16
+//
+//  Created by Amr Mohamad on 23/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
