31ef70448dee2a220cb01c2fa52b19198b7f50e2
AmrMohamad <amr.mohamed124124@gmail.com>
2023-04-24 19:14:52 +0200

    set ui Clima project

--- Clima-UIKit-iOS16/Clima-UIKit-iOS16/Controller/WeatherViewController.swift
+++ Clima-UIKit-iOS16/Clima-UIKit-iOS16/Controller/WeatherViewController.swift
@@ -23,6 +23,13 @@ extension WeatherViewController {
     func setUI(){
         view.addSubview(wthrUI.bGImage)
         view.addSubview(wthrUI.locationButton)
+        view.addSubview(wthrUI.searchButton)
+        view.addSubview(wthrUI.searchTextField)
+        view.addSubview(wthrUI.conditionImage)
+        view.addSubview(wthrUI.dgreeSignLabel)
+        view.addSubview(wthrUI.dgreeValueLabel)
+        view.addSubview(wthrUI.cityLabel)
+        
         
         let bGImageConstrains = [
             wthrUI.bGImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -38,7 +45,49 @@ extension WeatherViewController {
             wthrUI.locationButton.widthAnchor.constraint(equalToConstant: 40)
         ]
         
+        let searchButtonConstrains = [
+            wthrUI.searchButton.centerYAnchor.constraint(equalTo: wthrUI.locationButton.centerYAnchor),
+            wthrUI.searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            wthrUI.searchButton.heightAnchor.constraint(equalToConstant: 40),
+            wthrUI.searchButton.widthAnchor.constraint(equalToConstant: 40)
+        ]
+        
+        let searchTextFieldConstrains = [
+            wthrUI.searchTextField.centerYAnchor.constraint(equalTo: wthrUI.locationButton.centerYAnchor),
+            wthrUI.searchTextField.leadingAnchor.constraint(equalTo: wthrUI.locationButton.trailingAnchor, constant: 10),
+            wthrUI.searchTextField.trailingAnchor.constraint(equalTo: wthrUI.searchButton.leadingAnchor, constant: -10)
+        ]
+        
+        let conditionImageConstrains = [
+            wthrUI.conditionImage.trailingAnchor.constraint(equalTo: wthrUI.searchButton.trailingAnchor),
+            wthrUI.conditionImage.topAnchor.constraint(equalTo: wthrUI.searchButton.bottomAnchor, constant: 10),
+            wthrUI.conditionImage.heightAnchor.constraint(equalToConstant: 120),
+            wthrUI.conditionImage.widthAnchor.constraint(equalToConstant: 120)
+            
+        ]
+        
+        let dgreeSignLabelConstrains = [
+            wthrUI.dgreeSignLabel.trailingAnchor.constraint(equalTo: wthrUI.searchButton.trailingAnchor),
+            wthrUI.dgreeSignLabel.topAnchor.constraint(equalTo: wthrUI.conditionImage.bottomAnchor, constant: 10)
+        ]
+        
+        let dgreeValueLabelConstrains = [
+            wthrUI.dgreeValueLabel.centerYAnchor.constraint(equalTo: wthrUI.dgreeSignLabel.centerYAnchor),
+            wthrUI.dgreeValueLabel.trailingAnchor.constraint(equalTo: wthrUI.dgreeSignLabel.leadingAnchor, constant: 0)
+        ]
+        
+        let cityLabelConstrains = [
+            wthrUI.cityLabel.trailingAnchor.constraint(equalTo: wthrUI.searchButton.trailingAnchor),
+            wthrUI.cityLabel.topAnchor.constraint(equalTo: wthrUI.dgreeSignLabel.bottomAnchor, constant: 10)
+        ]
+        
         NSLayoutConstraint.activate(bGImageConstrains)
         NSLayoutConstraint.activate(locationButtonConstrains)
+        NSLayoutConstraint.activate(searchButtonConstrains)
+        NSLayoutConstraint.activate(searchTextFieldConstrains)
+        NSLayoutConstraint.activate(conditionImageConstrains)
+        NSLayoutConstraint.activate(dgreeSignLabelConstrains)
+        NSLayoutConstraint.activate(dgreeValueLabelConstrains)
+        NSLayoutConstraint.activate(cityLabelConstrains)
     }
 }
