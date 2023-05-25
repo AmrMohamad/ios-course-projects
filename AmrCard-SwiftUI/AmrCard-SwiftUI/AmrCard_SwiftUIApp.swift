b86ac07c202f2e68ada670ec2fa40c65fd101582
AmrMohamad <amr.mohamed124124@gmail.com>
2023-05-25 20:34:19 +0300

    Initialized AmrCard-SwiftUI app project

---
+++ AmrCard-SwiftUI/AmrCard-SwiftUI/AmrCard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmrCard_SwiftUIApp.swift
+//  AmrCard-SwiftUI
+//
+//  Created by Amr Mohamad on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AmrCard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
