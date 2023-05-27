e0126a63d0be081c2cbf3482803abb118274029d
AmrMohamad <amr.mohamed124124@gmail.com>
2023-05-27 15:39:47 +0300

    Initialized Dicee-SwiftUI app project

---
+++ Dicee-SwiftUI-iOS16/Dicee-SwiftUI-iOS16/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI-iOS16
+//
+//  Created by Amr Mohamad on 30/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
