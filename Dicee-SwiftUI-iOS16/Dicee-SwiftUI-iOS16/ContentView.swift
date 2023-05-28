3b5839e32f4bfacd3ca2b760ba3ab8beb9bf3924
AmrMohamad <amr.mohamed124124@gmail.com>
2023-05-28 17:16:51 +0300

    Created UI of  Dicee-SwiftUI app project

--- Dicee-SwiftUI-iOS16/Dicee-SwiftUI-iOS16/ContentView.swift
+++ Dicee-SwiftUI-iOS16/Dicee-SwiftUI-iOS16/ContentView.swift
@@ -9,15 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: 1)
+                    DiceView(n: 2)
+                }
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Text("Roll")
+                        .padding(.horizontal)
+                        .font(.system(size: 50))
+                        .foregroundColor(.white)
+                        .fontWeight(.heavy)
+                        
+                }
+                .background(Color.red)
+
+            }
         }
-        .padding()
     }
 }
 
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
