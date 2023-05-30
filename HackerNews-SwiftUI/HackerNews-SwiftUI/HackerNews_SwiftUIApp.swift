af2dcd548137594c576c2c577a76befd40731aab
AmrMohamad <amr.mohamed124124@gmail.com>
2023-05-30 20:38:08 +0300

    Initialized HeckerNews-SwiftUI app project

---
+++ HackerNews-SwiftUI/HackerNews-SwiftUI/HackerNews_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNews_SwiftUIApp.swift
+//  HackerNews-SwiftUI
+//
+//  Created by Amr Mohamad on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNews_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
