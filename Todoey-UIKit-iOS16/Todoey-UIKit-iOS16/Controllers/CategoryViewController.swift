1bc34154305100fcb4f6dbd5a7129848386c461b
AmrMohamad <amr.mohamed124124@gmail.com>
2023-07-02 18:07:57 +0300

    Create CategoryViewController of Todoey-UIKit-iOS16 project

---
+++ Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/CategoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryViewController.swift
+//  Todoey-UIKit-iOS16
+//
+//  Created by Amr Mohamad on 15/08/2023.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
