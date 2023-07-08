863d8ae68786888c7907e3ad437d19079cf2c6a2
AmrMohamad <amr.mohamed124124@gmail.com>
2023-07-08 18:23:16 +0300

    Initialize CoreData for Todoey-UIKit-iOS16 project

--- Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/CategoryViewController.swift
+++ Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/CategoryViewController.swift
@@ -6,25 +6,21 @@
 //
 
 import UIKit
+import CoreData
 
-class CategoryViewController: UIViewController {
-
+class CategoryViewController: UITableViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .brown
+        
     }
+//    
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        return UITableViewCell()
+//    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
