4ebe7ae9606a77db3ac97f0539716ffd9f77a4e7
AmrMohamad <amr.mohamed124124@gmail.com>
2023-07-27 18:25:50 +0300

    Add TableView to CategoryVC Todoey-UIKit-iOS16 project

--- Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/CategoryViewController.swift
+++ Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/CategoryViewController.swift
@@ -14,13 +14,13 @@ class CategoryViewController: UITableViewController{
         super.viewDidLoad()
         
     }
-//    
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1
-//    }
-//    
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        return UITableViewCell()
-//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
     
 }
