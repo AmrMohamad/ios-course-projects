06603c24261a66309bdd368b49069b2770a5e085
AmrMohamad <amr.mohamed124124@gmail.com>
2023-08-03 17:46:47 +0300

    Delete CoreData Todoey-UIKit-iOS16 project

--- Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/CategoryViewController.swift
+++ Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/CategoryViewController.swift
@@ -10,94 +10,94 @@ import CoreData
 
 class CategoryViewController: UITableViewController{
     
-    var categories = [Category]()
-    let context    = (
-        UIApplication
-            .shared
-            .delegate as! AppDelegate
-    ).persistentContainer
-        .viewContext
+//    var categories = [Category]()
+//    let context    = (
+//        UIApplication
+//            .shared
+//            .delegate as! AppDelegate
+//    ).persistentContainer
+//        .viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        BigTitleNavigationBar(title: "Todoey")
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: .add,
-            target: self,
-            action: #selector(addCategoryButtonPressed)
-        )
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CategoryCell")
-        
-        loadCategories()
+//        BigTitleNavigationBar(title: "Todoey")
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(
+//            barButtonSystemItem: .add,
+//            target: self,
+//            action: #selector(addCategoryButtonPressed)
+//        )
+//
+//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CategoryCell")
+//
+//        loadCategories()
     }
     
     //MARK: - DataSource Methods
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return categories.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        let category = categories[indexPath.row]
-        cell.textLabel?.text = category.name
-        return cell
-    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//        return categories.count
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+//        let category = categories[indexPath.row]
+//        cell.textLabel?.text = category.name
+//        return cell
+//    }
     
     //MARK: - Add New Cetegories
     
-    @objc func addCategoryButtonPressed(_ sender: UIBarButtonItem){
-        let alert = UIAlertController(
-            title: "Add New Category",
-            message: nil,
-            preferredStyle: .alert
-        )
-        var textFieldAlert = UITextField()
-        let action = UIAlertAction(
-            title: "Add Category",
-            style: .default) { action in
-                let newCategroy = Category(context: self.context)
-                newCategroy.name = textFieldAlert.text!
-                self.categories.append(newCategroy)
-                self.save()
-            }
-        alert.addTextField { textField in
-            textField.placeholder = "Name the new category"
-            textFieldAlert = textField
-        }
-        alert.addAction(action)
-        present(alert, animated: true)
-    }
+//    @objc func addCategoryButtonPressed(_ sender: UIBarButtonItem){
+//        let alert = UIAlertController(
+//            title: "Add New Category",
+//            message: nil,
+//            preferredStyle: .alert
+//        )
+//        var textFieldAlert = UITextField()
+//        let action = UIAlertAction(
+//            title: "Add Category",
+//            style: .default) { action in
+//                let newCategroy = Category(context: self.context)
+//                newCategroy.name = textFieldAlert.text!
+//                self.categories.append(newCategroy)
+//                self.save()
+//            }
+//        alert.addTextField { textField in
+//            textField.placeholder = "Name the new category"
+//            textFieldAlert = textField
+//        }
+//        alert.addAction(action)
+//        present(alert, animated: true)
+//    }
     
     //MARK: - TabView Delegate Methods
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let todoVC = TodoListViewController()
-        todoVC.selectedCategory = categories[indexPath.row]
-        self.navigationController?.pushViewController(todoVC, animated: true)
-
-    }
-    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let todoVC = TodoListViewController()
+//        todoVC.selectedCategory = categories[indexPath.row]
+//        self.navigationController?.pushViewController(todoVC, animated: true)
+//
+//    }
+//
     //MARK: - Data Operations Methods
     
-    func save(){
-        do{
-            try context.save()
-        } catch {
-            print("Error Saving Data \(error)")
-        }
-        tableView.reloadData()
-    }
-    
-    func loadCategories (){
-        let request: NSFetchRequest<Category> = Category.fetchRequest()
-        do{
-            categories = try context.fetch(request)
-        } catch {
-            print("Error loading Data \(error)")
-        }
-        tableView.reloadData()
-    }
+//    func save(){
+//        do{
+//            try context.save()
+//        } catch {
+//            print("Error Saving Data \(error)")
+//        }
+//        tableView.reloadData()
+//    }
+//    
+//    func loadCategories (){
+//        let request: NSFetchRequest<Category> = Category.fetchRequest()
+//        do{
+//            categories = try context.fetch(request)
+//        } catch {
+//            print("Error loading Data \(error)")
+//        }
+//        tableView.reloadData()
+//    }
 }
--- Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/TodoListViewController.swift
+++ Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/TodoListViewController.swift
@@ -10,123 +10,123 @@ import CoreData
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = [Item]()
-    let context   = (
-        UIApplication
-            .shared
-            .delegate as! AppDelegate
-    ).persistentContainer
-        .viewContext
-    
-    var selectedCategory: Category? {
-        didSet{
-            loadItems()
-            BigTitleNavigationBar(title: selectedCategory!.name!)
-        }
-    }
+//    var itemArray = [Item]()
+//    let context   = (
+//        UIApplication
+//            .shared
+//            .delegate as! AppDelegate
+//    ).persistentContainer
+//        .viewContext
+//
+//    var selectedCategory: Category? {
+//        didSet{
+//            loadItems()
+//            BigTitleNavigationBar(title: selectedCategory!.name!)
+//        }
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem
-            .rightBarButtonItem = UIBarButtonItem(
-                barButtonSystemItem: .add,
-                target: self,
-                action: #selector(addItemPressed)
-            )
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ToDoItemCell")
+//        navigationItem
+//            .rightBarButtonItem = UIBarButtonItem(
+//                barButtonSystemItem: .add,
+//                target: self,
+//                action: #selector(addItemPressed)
+//            )
+//
+//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ToDoItemCell")
         
     }
     
     
     //MARK: - DataSource Methods
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        let item = itemArray[indexPath.row]
-        cell.textLabel?.text = item.title
-        cell.accessoryType = item.done ? .checkmark : .none
-        return cell
-    }
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return itemArray.count
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+//        let item = itemArray[indexPath.row]
+//        cell.textLabel?.text = item.title
+//        cell.accessoryType = item.done ? .checkmark : .none
+//        return cell
+//    }
     //
     //    //MARK: - Deleagte Methods
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let item = itemArray[indexPath.row]
-        
-//        How to Delete Items : 👇🏻
-//        context.delete(item)
-//        itemArray.remove(at: indexPath.row)
-        
-        item.done = !item.done
-        saveItem()
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//
+//        let item = itemArray[indexPath.row]
+//
+////        How to Delete Items : 👇🏻
+////        context.delete(item)
+////        itemArray.remove(at: indexPath.row)
+//
+//        item.done = !item.done
+//        saveItem()
+//
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
     
     //MARK: - Add item
     
-    @objc func addItemPressed(){
-        var alertOfTextField = UITextField()
-        
-        let alert = UIAlertController(
-            title: "Add New ToDo Item",
-            message: "",
-            preferredStyle: .alert
-        )
-        alert.addTextField { alertTextField in
-            alertTextField.placeholder = "Create New Item"
-            alertOfTextField = alertTextField
-        }
-        let action = UIAlertAction(
-            title: "Add Item",
-            style: .default
-        ) { action in
-            let newItem = Item(context: self.context)
-            newItem.title = alertOfTextField.text!
-            newItem.done  = false
-            newItem.parentCategory = self.selectedCategory
-            self.itemArray.append(newItem)
-            self.saveItem()
-        }
-        alert.addAction(action)
-        present(alert, animated: true)
-    }
+//    @objc func addItemPressed(){
+//        var alertOfTextField = UITextField()
+//
+//        let alert = UIAlertController(
+//            title: "Add New ToDo Item",
+//            message: "",
+//            preferredStyle: .alert
+//        )
+//        alert.addTextField { alertTextField in
+//            alertTextField.placeholder = "Create New Item"
+//            alertOfTextField = alertTextField
+//        }
+//        let action = UIAlertAction(
+//            title: "Add Item",
+//            style: .default
+//        ) { action in
+//            let newItem = Item(context: self.context)
+//            newItem.title = alertOfTextField.text!
+//            newItem.done  = false
+//            newItem.parentCategory = self.selectedCategory
+//            self.itemArray.append(newItem)
+//            self.saveItem()
+//        }
+//        alert.addAction(action)
+//        present(alert, animated: true)
+//    }
     
     //MARK: - Saving/Writing Custom Data using NSCoder
     
-    func saveItem(){
-        
-        do {
-            try context.save()
-        } catch {
-            print("Error with saving data \(error)")
-        }
-        tableView.reloadData()
-    }
-    
+//    func saveItem(){
+//
+//        do {
+//            try context.save()
+//        } catch {
+//            print("Error with saving data \(error)")
+//        }
+//        tableView.reloadData()
+//    }
+//
     
     //MARK: - Loading/Reading Custom Data using NSCoder
-    
-    func loadItems(
-        with request:NSFetchRequest<Item> = Item.fetchRequest(),
-        predicate: NSPredicate? = nil
-    ){
-        
-        do {
-            let categoryPredicate = NSPredicate(
-                format: "parentCategory.name MATCHES %@",
-                selectedCategory!.name!
-            )
-            request.predicate = categoryPredicate
-            itemArray = try context.fetch(request)
-        } catch {
-            print("Error with Fetching data By CoreData \(error)")
-        }
-        tableView.reloadData()
-    }
+//
+//    func loadItems(
+//        with request:NSFetchRequest<Item> = Item.fetchRequest(),
+//        predicate: NSPredicate? = nil
+//    ){
+//
+//        do {
+//            let categoryPredicate = NSPredicate(
+//                format: "parentCategory.name MATCHES %@",
+//                selectedCategory!.name!
+//            )
+//            request.predicate = categoryPredicate
+//            itemArray = try context.fetch(request)
+//        } catch {
+//            print("Error with Fetching data By CoreData \(error)")
+//        }
+//        tableView.reloadData()
+//    }
 }
 
