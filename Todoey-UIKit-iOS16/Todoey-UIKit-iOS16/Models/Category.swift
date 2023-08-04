3c688493caea95f6c6f235b6726e691601ecb42c
AmrMohamad <amr.mohamed124124@gmail.com>
2023-08-04 18:07:43 +0300

    Initialize Realm DB Todoey-UIKit-iOS16 project

---
+++ Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Models/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Todoey-UIKit-iOS16
+//
+//  Created by Amr Mohamad on 16/08/2023.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    
+}
+
---
+++ Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Models/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoey-UIKit-iOS16
+//
+//  Created by Amr Mohamad on 16/08/2023.
+//
+
+import Foundation
+import RealmSwift
+
+class Item: Object {
+    
+}
