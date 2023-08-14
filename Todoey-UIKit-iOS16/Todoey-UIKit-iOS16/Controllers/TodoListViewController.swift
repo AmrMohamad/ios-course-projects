cd53436fc324bd08829b9d12302c734e30835541
AmrMohamad <amr.mohamed124124@gmail.com>
2023-08-14 19:59:13 +0300

    Delete an Item just by swiping the cell from right to left Todoey-UIKit-iOS16 project

--- Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/TodoListViewController.swift
+++ Todoey-UIKit-iOS16/Todoey-UIKit-iOS16/Controllers/TodoListViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 import RealmSwift
+import SwipeCellKit
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: SwipeTableViewController {
     
     var todoItems: Results<Item>?
     let realm = try! Realm()
@@ -30,7 +31,7 @@ class TodoListViewController: UITableViewController {
                 action: #selector(addItemPressed)
             )
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ToDoItemCell")
+        tableView.register(SwipeTableViewCell.self, forCellReuseIdentifier: "ToDoItemCell")
         
         let search = UISearchController(searchResultsController: nil)
         search.delegate = self
@@ -46,10 +47,8 @@ class TodoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: "ToDoItemCell",
-            for: indexPath
-        )
+        super.identifierOfCell = "ToDoItemCell"
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if let item = todoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
@@ -132,6 +131,18 @@ class TodoListViewController: UITableViewController {
     func loadItems(){
         todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
         tableView.reloadData()
+    }
+    
+    override func updateModel(at indexPath: IndexPath) {
+        do {
+            if let itemForDeletion = todoItems?[indexPath.row] {
+                try realm.write({
+                    realm.delete(itemForDeletion)
+                })
+            }
+        }catch{
+            print("Error with deleting \(error)")
+        }
     }
 }
 
