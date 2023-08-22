d848c705ded1fc7e53dced51a01cf115a7669b6e
AmrMohamad <amr.mohamed124124@gmail.com>
2023-08-22 22:57:19 +0300

    Create InspoQuotes-iOS16 project

---
+++ InspoQuotes-iOS16/InspoQuotes-iOS16/QuoteTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InspoQuotes-iOS16
+//
+//  Created by Amr Mohamad on 22/08/2023.
+//
+
+import UIKit
+
+class QuoteTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
