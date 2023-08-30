2367b68d122a9bc6e9654880ec917f476106c5e7
AmrMohamad <amr.mohamed124124@gmail.com>
2023-08-30 15:24:18 +0300

    Type multiple digits as number value-Calculator app

--- Advanced Swift part 2/Calculator-iOS16/Calculator-iOS16/ViewController.swift
+++ Advanced Swift part 2/Calculator-iOS16/Calculator-iOS16/ViewController.swift
@@ -11,18 +11,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
+    var isFinishedTypingNumber: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
+        isFinishedTypingNumber = true
+        displayLabel.text = "0"
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
-    
 }
-
