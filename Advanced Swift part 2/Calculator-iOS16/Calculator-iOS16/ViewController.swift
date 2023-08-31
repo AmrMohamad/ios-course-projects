c2c431d9bc3ca95fee69a07cb8ac06ed033d3de0
AmrMohamad <amr.mohamed124124@gmail.com>
2023-08-31 18:13:48 +0300

    Fix entering decimal number Calculator app

--- Advanced Swift part 2/Calculator-iOS16/Calculator-iOS16/ViewController.swift
+++ Advanced Swift part 2/Calculator-iOS16/Calculator-iOS16/ViewController.swift
@@ -22,22 +22,23 @@ class ViewController: UIViewController {
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
         //        displayLabel.text = "0"
-        
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert string of label to a Double")
-        }
-        if let calMethod = sender.currentTitle {
-            switch calMethod {
-            case "+/-":
-                displayLabel.text = number == 0.0 ? "0" : String(number * -1)
-            case "AC":
-                displayLabel.text = "0"
-            case "%":
-                displayLabel.text = number == 0.0 ? "0" : String(number * 0.01)
-            default:
-                displayLabel.text = "0"
+        if displayLabel.text! != "."{
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert string of label to a Double")
             }
-        }
+            if let calMethod = sender.currentTitle {
+                switch calMethod {
+                case "+/-":
+                    displayLabel.text = number == 0.0 ? "0" : String(number * -1)
+                case "AC":
+                    displayLabel.text = "0"
+                case "%":
+                    displayLabel.text = number == 0.0 ? "0" : String(number * 0.01)
+                default:
+                    displayLabel.text = "0"
+                }
+            }
+        } else {return}
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
@@ -46,6 +47,15 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
+                if displayLabel.text! != "."{
+                    if numValue == "." {
+                        guard let currentDisplayValue = Double(displayLabel.text!) else {
+                            fatalError("Cannot convert string of label to a Double")
+                        }
+                        let isInt = floor(currentDisplayValue) == currentDisplayValue
+                        if !isInt {return}
+                    }
+                } else {return}
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
